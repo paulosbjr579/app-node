ef10c7e262a7e57ebe17f983bc32d2652508b87d
paulo <psbj579@gmail.com>
2020-02-28 20:32:36 -0300

    fix: change swagger find sales

--- app/src/api/routes/sales.js
+++ app/src/api/routes/sales.js
@@ -108,11 +108,113 @@ router.get('/:id', controller.findbyid);
  */
 router.put('/:id', controller.updata);
 
-
+/**
+ * @swagger
+ * /sales/{Id}:
+ *   delete:
+ *     tags:
+ *     - "Sales"
+ *     summary: "Delete sales"
+ *     description: "This can only be done by the logged in sales."
+ *     operationId: "deleteSalesid"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: Id
+ *       in: "path"
+ *       description: "The name that needs to be deleted"
+ *       required: true
+ *       type: "integer"
+ *     responses:
+ *       400:
+ *         description: "Invalid sales supplied"
+ *       404:
+ *         description: "sales not found"
+ */
 router.delete('/:id', controller.delete);
+
+/**
+ * @swagger
+ * /sales/findIdClient/{id}:
+ *   get:
+ *     tags:
+ *     - "Sales"
+ *     summary: "Get Sales by id client"
+ *     description: ""
+ *     operationId: "getSalesByIdClient"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: "id"
+ *       in: "path"
+ *       description: "The name that needs to be fetched. Use id client for testing. "
+ *       required: true
+ *       type: "integer"
+ *     responses:
+ *       200:
+ *         description: "successful operation"
+ *       400:
+ *         description: "Invalid id supplied"
+ *       404:
+ *         description: "Sales not found"
+ */
 router.get('/findIdClient/:id', controller.findIdClient);
+
+/**
+ * @swagger
+ * /sales/findIdSales/{id}:
+ *   get:
+ *     tags:
+ *     - "Sales"
+ *     summary: "Get Sales e Salesitens by id "
+ *     description: ""
+ *     operationId: "getSalesItensSalesById"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: "id"
+ *       in: "path"
+ *       description: "The name that needs to be fetched. Use id Sales for testing. "
+ *       required: true
+ *       type: "integer"
+ *     responses:
+ *       200:
+ *         description: "successful operation"
+ *       400:
+ *         description: "Invalid id supplied"
+ *       404:
+ *         description: "Sales not found"
+ */
 router.get('/findIdSales/:id', controller.findIdSales);
+
+/**
+ * @swagger
+ * /sales/findItenIdSales/{id}:
+ *   get:
+ *     tags:
+ *     - "Sales"
+ *     summary: "Get SalesItens by id client"
+ *     description: ""
+ *     operationId: "getSalesItensSalesById"
+ *     produces:
+ *     - "application/json"
+ *     parameters:
+ *     - name: "id"
+ *       in: "path"
+ *       description: "The name that needs to be fetched. Use id Sales for testing. "
+ *       required: true
+ *       type: "integer"
+ *     responses:
+ *       200:
+ *         description: "successful operation"
+ *       400:
+ *         description: "Invalid id supplied"
+ *       404:
+ *         description: "Sales not found"
+ */
 router.get('/findItenIdSales/:id', controller.findItenIdSales);
+
+
 router.delete('/', controller.deleteAll);
 
 module.exports = router;
